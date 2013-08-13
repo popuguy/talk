796d307461ad1fb24309ca89833651eab5dc7bb5
Paul Cantrell <paulcantr@gmail.com>
2013-08-13 00:16:31 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Talk::Application.config.secret_key_base = 'f4f4b3521775954e12b38b6439f038ba5fe1b02ba3f7d0f4af4a2387b3d50925fe45cd27e5a29843f0ddba510bb3efb43819a350c483605b1b29615d41e839b4'
